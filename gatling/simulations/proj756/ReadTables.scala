18a629c65e1f50829df6e7e31002442579732205
Brendan <baa30@sfu.ca>
2022-03-28 18:18:07 -0700

    Gatling, Grafana, S3
    - Made some small changes to get grafana gatling and the playlist service.
    - Added k8s.mak and eks.mak to .gitignore

--- gatling/simulations/proj756/ReadTables.scala
+++ gatling/simulations/proj756/ReadTables.scala
@@ -60,7 +60,7 @@ object RPlaylist {
   val rplaylist = forever("i") {
     feed(feeder)
     .exec(http("RPlaylist ${i}")
-      .get("/api/v1/playlist/${UUID}"))
+      .get("/api/v1/playlist/"))
       .pause(1)
   }
 }
